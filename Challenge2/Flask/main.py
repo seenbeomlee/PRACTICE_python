31b1d296bdd9245c22f11d8b2e162b36bc2a23e9
Seenbeom Lee <mail@seenblee.com>
2020-09-01 23:11:21 +0900

    #Day9 / 1.87
    1/ saving rendered result as csv file

--- Challenge2/Flask/main.py
+++ Challenge2/Flask/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
 
@@ -25,5 +26,20 @@ def report():
   else:
     return redirect("/")
   return render_template("report.html", searchingBy=word, resultsNumber = len(jobs), jobs=jobs, potato='imPotato')
+
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+  except:
+    return redirect("/")
 
 app.run(host="0.0.0.0")
